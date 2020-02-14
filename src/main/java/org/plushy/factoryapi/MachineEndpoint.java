cd368ece258bbc9100bb422ead25644c2b2974b5
dariuszlee <dariuszlee93@gmail.com>
2020-02-14 09:25:49 +0100

    Machine endpoint configured

--- src/main/java/org/plushy/factoryapi/MachineEndpoint.java
+++ src/main/java/org/plushy/factoryapi/MachineEndpoint.java
@@ -1,12 +1,15 @@
 package org.plushy.factoryapi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MachineEndpoint {
 
-    @GetMapping("/employees")
+    @GetMapping("/machines")
     public List<String> all() {
         return new ArrayList<String>();
     }
